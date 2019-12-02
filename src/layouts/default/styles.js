7d5fdac40afb2ed198096cebab45dc7a9637f1e6
Igor Trindade <igorhideki@gmail.com>
2019-12-02 06:25:24 -0400

    Add layout default

---
+++ src/layouts/default/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  min-height: 100%;
+  background: linear-gradient(-45deg, #fbfdff, #e7f3ff);
+`;
+
+export const Container = styled.div`
+  padding: 0 30px;
+  overflow: auto;
+`;
+
+export const Content = styled.div`
+  max-width: 940px;
+  margin: 52px auto;
+`;
