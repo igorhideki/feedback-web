b7f1cc0487bef65cf781a6e2e1a9c63db58e2bdf
Igor Trindade <igorhideki@gmail.com>
2019-12-02 20:48:10 -0400

    Add component card

---
+++ src/components/Card/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Container } from './styles';
+
+export default function Card({ children }) {
+  return <Container>{children}</Container>;
+}
+
+Card.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.element, PropTypes.string])
+    .isRequired,
+};
---
+++ src/components/Card/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: #fff;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+  padding: 16px;
+`;
