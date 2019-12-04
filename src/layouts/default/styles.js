ddd8c972393b2d9dbb86d1ffeaa66d1edc879cb2
Igor Trindade <igorhideki@gmail.com>
2019-12-04 04:53:41 -0400

    Update styles default layout

--- src/layouts/default/styles.js
+++ src/layouts/default/styles.js
@@ -3,11 +3,14 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   min-height: 100%;
   background: #f8f9fe;
+  display: flex;
+  flex-direction: column;
 `;
 
 export const Container = styled.div`
   padding: 0 30px;
   overflow: auto;
+  flex: 1;
 `;
 
 export const Content = styled.div`
