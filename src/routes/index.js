13e3debc3381c484e0e7acdce1c556abd751f07c
Igor Trindade <igorhideki@gmail.com>
2019-12-04 04:56:14 -0400

    Add routes for feedback and review

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,16 @@ import { Switch } from 'react-router-dom';
 import Route from './Route';
 import SignIn from '../pages/SignIn';
 import Dashboard from '../pages/Dashboard';
+import FeedBack from '../pages/Feedback';
+import Review from '../pages/Review';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" component={SignIn} exact />
       <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/feedback" component={FeedBack} isPrivate />
+      <Route path="/review" component={Review} isPrivate />
       <Route path="/" component={() => <h1>Error 404. Page not found</h1>} />
     </Switch>
   );
